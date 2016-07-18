b8a57ba3c72cb2d7ac97810b3418f8c25d690239
Nicky Out <nicky.out@webprint.nl>
2016-07-18 14:52:23 +0200

    Append 2 newlines to table for convenience

--- makeTable.js
+++ makeTable.js
@@ -5,7 +5,7 @@ var commitID = 'abc';
 if (process.argv.length >= 4) {
     logFilePath = process.argv[2];
     commitID = process.argv[3];
-    console.log(logToTable(fs.readFileSync(logFilePath, 'utf-8'), commitID));
+    console.log(logToTable(fs.readFileSync(logFilePath, 'utf-8'), commitID) + '\n\n');
 } else {
     console.log('Usage: ' + process.argv.slice(0,2).join(' ') + ' <logFile> <commitID>');
 }
