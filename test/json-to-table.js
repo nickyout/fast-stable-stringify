fe0661e676bf1fc56d0bd9c135ecb117afdce134
Nicky Out <nicky.out@webprint.nl>
2017-02-15 16:48:35 +0100

    Update test

--- test/json-to-table.js
+++ test/json-to-table.js
@@ -2,9 +2,11 @@ var jsonToTable = require('../util/json-to-table');
 var testLog = require('../fixtures/log-result.json');
 var fs = require('fs');
 var tableResult = fs.readFileSync('fixtures/table.txt', 'utf-8');
+var assert = require('assert');
 
 suite("json-to-table", function() {
 	test("conversion to table", function() {
-		console.log(jsonToTable(testLog, ["nickyout/fast-stable-stringify"], ["substack/json-stable-stringify"]));
+        var tableString = jsonToTable(testLog, ["nickyout/fast-stable-stringify"], ["substack/json-stable-stringify"]);
+        assert.equal(tableString, tableResult);
 	});
 });
