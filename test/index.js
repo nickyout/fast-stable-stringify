6f9944042e0423ba8f388b265bbbcabdda946a25
Nicky Out <nicky.out@webprint.nl>
2017-09-06 18:08:05 +0200

    Moved valid and fastest into one file. Don't see an easy way to declare benchmark and test cases next to each other

--- test/index.js
+++ test/index.js
@@ -1,5 +1,53 @@
-require('./valid');
-require('./fastest');
-//require("./log-to-json");
-//require('./get-git-hash');
-//require('./json-to-table');
+var assert = require("assert");
+var indexStringify = require('../index');
+var jsonStableStringify = require('json-stable-stringify');
+var fasterStableStringify = require('faster-stable-stringify');
+var fastStableStringify = require('fast-stable-stringify');
+
+var data = require("../fixtures/index").input;
+var dataLength = JSON.stringify(data).length;
+var eachRecursive = require('./util/eachRecursive');
+
+function isValid(input, myStringify, theirStringify) {
+	var numComparisons = 0;
+	eachRecursive(input, function (val, path) {
+		var mine = myStringify(val);
+		var expectedVal = theirStringify(val);
+		assert.equal(mine, expectedVal);
+		numComparisons++;
+	});
+	assert.equal(numComparisons, 606);
+}
+
+suite("Benchmark", function() {
+
+	// This needs to be true before anything else
+	console.log('Checking index validity...');
+	isValid(data, indexStringify, jsonStableStringify);
+	console.log('Checking index validity success');
+
+	benchmark('index', function () {
+		var result = indexStringify(data);
+		assert.equal(result.length, dataLength);
+	});
+
+	benchmark('native', function () {
+		var result = JSON.stringify(data);
+		assert.equal(result.length, dataLength);
+	});
+
+	benchmark('json-stable-stringify', function () {
+		var result = jsonStableStringify(data);
+		assert.equal(result.length, dataLength);
+	});
+
+	benchmark('faster-stable-stringify', function () {
+		var result = fasterStableStringify(data);
+		assert.equal(result.length, dataLength);
+	});
+
+	benchmark('fast-stable-stringify', function () {
+		var result = fastStableStringify(data);
+		assert.equal(result.length, dataLength);
+	});
+});
